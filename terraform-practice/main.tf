8ad998133a1546b0791417a6f9f75d6a9dc44908
Kenchan <ken@example.com>
2026-04-14 23:17:35 +0900

    add terraform init files

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-12345678"
   instance_type = "t3.small"
