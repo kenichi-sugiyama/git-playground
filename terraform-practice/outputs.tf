5d75a8e1e5ef123d5200643682e6bf029387f12b
Kenchan <ken@example.com>
2026-05-25 15:14:42 +0900

    add instance type output

--- terraform-practice/outputs.tf
+++ terraform-practice/outputs.tf
@@ -1,4 +1,4 @@
-output "instance_name" {
-  value       = var.instance_name
-  description = "Name tag of the EC2 instance"
+output "instance_type" {
+  value       = var.instance_type
+  description = "EC2 instance type"
 }
